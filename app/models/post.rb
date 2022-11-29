27c50fe974d3dea3c842e8a16700484f1b8f2886
Derrick Nyasio <dnyasio2014@gmail.com>
2022-11-29 19:04:02 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum:250 }
+  validates :summary, length: { maximum:250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_it_clickbait
+
+  def is_it_clickbait
+    values = [/Won't Believe/,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i]
+    if values.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
